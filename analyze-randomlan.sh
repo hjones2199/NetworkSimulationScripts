7f99be34ee973d752284d92b2366df6187f9d617
hjones2199 <hjones2199@gmail.com>
2019-08-01 23:15:16 -0500

    Modified analysis scripts to check packets over time

--- analyze-randomlan.sh
+++ analyze-randomlan.sh
@@ -10,10 +10,50 @@ grep -c "^r.*" out.tr
 
 echo "Number of packets dropped: "
 grep -c "^d.*" out.tr
+echo
 
 # first 6th
-#echo ""
-#split -n l/1/6 out.tr | grep -c "$"
+echo "Receives in the first 6th: "
+split -n l/1/6 out.tr | grep -c "^r.*"
+echo "Drops in the first 6th: "
+split -n l/1/6 out.tr | grep -c "^d.*"
+echo
+
+# second 6th
+echo "Receives in the second 6th: "
+split -n l/2/6 out.tr | grep -c "^r.*"
+echo "Drops in the second 6th: "
+split -n l/2/6 out.tr | grep -c "^d.*"
+echo
+
+# third 6th
+echo "Receives in the third 6th: "
+split -n l/3/6 out.tr | grep -c "^r.*"
+echo "Drops in the third 6th: "
+split -n l/3/6 out.tr | grep -c "^d.*"
+echo
+
+# fourth 6th
+echo "Receives in the fourth 6th: "
+split -n l/4/6 out.tr | grep -c "^r.*"
+echo "Drops in the fourth 6th: "
+split -n l/4/6 out.tr | grep -c "^d.*"
+echo
+
+# fifth 6th
+echo "Receives in the fifth 6th: "
+split -n l/5/6 out.tr | grep -c "^r.*"
+echo "Drops in the fifth 6th: "
+split -n l/5/6 out.tr | grep -c "^d.*"
+echo
+
+# sixth 6th
+echo "Receives in the sixth 6th: "
+split -n l/6/6 out.tr | grep -c "^r.*"
+echo "Drops in the sixth 6th: "
+split -n l/6/6 out.tr | grep -c "^d.*"
+
+
 
 #echo "last quarter of file receives:"
 #split -n l/4/4 out.tr | grep -c "^r.*"
--- analyze-wirelessnet.sh
+++ analyze-wirelessnet.sh
@@ -1,5 +1,7 @@
-#! /bin/sh
+#! /bin/bash
 
+
+# totals
 echo "Total number of packets sent: "
 grep -c "^[rd].*" wirelessnet.tr
 
@@ -8,3 +10,50 @@ grep -c "^r.*" wirelessnet.tr
 
 echo "Number of packets dropped: "
 grep -c "^d.*" wirelessnet.tr
+echo
+
+# first 6th
+echo "Receives in the first 6th: "
+split -n l/1/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the first 6th: "
+split -n l/1/6 wirelessnet.tr | grep -c "^d.*"
+echo
+
+# second 6th
+echo "Receives in the second 6th: "
+split -n l/2/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the second 6th: "
+split -n l/2/6 wirelessnet.tr | grep -c "^d.*"
+echo
+
+# third 6th
+echo "Receives in the third 6th: "
+split -n l/3/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the third 6th: "
+split -n l/3/6 wirelessnet.tr | grep -c "^d.*"
+echo
+
+# fourth 6th
+echo "Receives in the fourth 6th: "
+split -n l/4/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the fourth 6th: "
+split -n l/4/6 wirelessnet.tr | grep -c "^d.*"
+echo
+
+# fifth 6th
+echo "Receives in the fifth 6th: "
+split -n l/5/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the fifth 6th: "
+split -n l/5/6 wirelessnet.tr | grep -c "^d.*"
+echo
+
+# sixth 6th
+echo "Receives in the sixth 6th: "
+split -n l/6/6 wirelessnet.tr | grep -c "^r.*"
+echo "Drops in the sixth 6th: "
+split -n l/6/6 wirelessnet.tr | grep -c "^d.*"
+
+
+
+#echo "last quarter of file receives:"
+#split -n l/4/4 wirelessnet.tr | grep -c "^r.*"
